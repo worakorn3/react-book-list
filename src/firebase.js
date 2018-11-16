15f9c28b95d7b14c2fb228006288f639502e537f
worakorn3 <worakorn.wu@ku.th>
2018-11-17 00:01:25 +0700

    projectId to string

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ const config = {
     apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
     authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIRESTORE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
+    projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID+"",
     storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID
 };
